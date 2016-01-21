d6d13b8b84f513bfe3212e4a655b25d03bfab506
Joel Humberto Gómez Paredes <elmaildeldezkareid@gmail.com>
2016-01-21 16:37:14 -0600

    Gulp template incorporado

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -8,10 +8,25 @@ var gulp        = require('gulp'),
     gutil       = require('gulp-util'),
     ngAnnotate  = require('gulp-ng-annotate'),
     gulpif      = require('gulp-if'),
-    templateCache = require('gulp-angular-templatecache');
+    templateCache = require('gulp-angular-templatecache'),
+    template = require('gulp-template');
 
-var cssFiles = require("./bowercss.json");
-var jsFiles = require("./bowerjs.json");
+var cssFiles = require("./listAssets/bowercss.json");
+var jsFiles = require("./listAssets/bowerjs.json");
+
+var enviroments = {
+  "development" : {
+  },
+  "production" : {
+  }
+}
+
+var enviroment = enviroments["development"];
+if (process.argv.length > 2){
+  if (process.argv[2] == "production") {
+    enviroment = enviroments["production"]
+  }
+};
 
 gulp.task('connect', function () {
   connect.server({
@@ -20,6 +35,8 @@ gulp.task('connect', function () {
     livereload: true                                                                                                                                                                                                                           
   });
 });
+
+
 
 gulp.task('templates', function () {
   return gulp.src(['app/**/*.html', 'app/**/*.jade'])
@@ -30,54 +47,74 @@ gulp.task('templates', function () {
       module: 'templates',
       standalone: true
     }))
-    .pipe(gulp.dest('build/js'))
+    .pipe(gulp.dest('build/assets/js'))
     .pipe(connect.reload());
 });
 
 var fontsDirectory = [
   'bower_components/font-awesome/fonts/*.*',
-  'bower_components/fontawesome/fonts/*.*',
   'bower_components/simple-line-icons/fonts/*.*' 
 ];
 
 gulp.task('copyfonts',function(){
   gulp.src(fontsDirectory, { base: 'bower_components' })
-  .pipe(gulp.dest('build/css'));
+  .pipe(gulp.dest('build/assets'));
 });
 
+gulp.task('copyImages',function(){
+  return gulp.src(['app/assets/img/**/.*', '!app/assets/img/.gitkeep'])
+  .pipe(gulp.dest('build/assets/img'));
+});
 
-gulp.task('minify-css', function () {
+gulp.task('minify-vendor-css', function () {
     return gulp.src(cssFiles)
         .pipe(concatCss('vendor.css'))
-        .pipe(gulp.dest('build/css'))
+        .pipe(gulp.dest('build/assets/css'))
 });
 
-gulp.task('estilos', function(){
+gulp.task('minify-own-styles', function(){
   return gulp.src("app/assets/styles/**/*.css")
       .pipe(concatCss('app.css'))
-      .pipe(gulp.dest('build/css'))
+      .pipe(gulp.dest('build/assets/css'))
 });
 
-gulp.task('minify-js', function() {
+gulp.task('minify-vendor-js', function() {
   return gulp.src(jsFiles)
     .pipe(concat('vendor.min.js'))
-    .pipe(gulp.dest('build/js/'))
+    .pipe(gulp.dest('build/assets/js/'))
 });
 
-gulp.task('coffee', function() {
-  gulp.src(['app/directives/**/*.coffee', 'app/modules/*.module.coffee' ,'app/modules/**/*.coffee','app/js/*.coffee'])
-    .pipe(coffee({bare: true}).on('error', gutil.log))
+gulp.task('minify-own-sources', function() {
+  gulp.src(['app/directives/**/*.coffee', 'app/modules/*.module.coffee' ,'app/modules/**/*.coffee','app/js/*.coffee', 'app/js/*.js'])
+    .pipe(
+      gulpif(/[.]coffee$/, coffee({bare: true}).on('error', gutil.log)
+    ))
+    /* Uncomment if you need make a template
+    .pipe(
+      gulpif(/app.js$/, template({baseUrl: enviroment.baseUrl })
+    ))*/
     .pipe(ngAnnotate())
     .pipe(concat('app.min.js'))
-    .pipe(gulp.dest("build/js/"))
+    .pipe(gulp.dest("build/assets/js/"))
     .pipe(connect.reload())
 });
 
+
+gulp.task('index', function(){
+  return gulp.src('app/index.jade')
+    //.pipe(template({base: enviroment.base })) Uncomment if you gonna use a template
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest('build/'));
+});
  
 gulp.task('watch', function () {
-  gulp.watch(['./app/**/*.coffee'], ['coffee']);
-  gulp.watch(['./app/assets/styles/**/*.css'],['estilos']);
-  gulp.watch(['./app/**/views/*.html', './app/**/views/*.jade'], ['templates']);
+  gulp.watch(['./app/**/*.coffee','./app/js/*.js'], ['minify-own-sources']);
+  gulp.watch(['./app/assets/css/**/*.css'],['minify-own-styles']);
+  gulp.watch(['./app/**/*.html', './app/**/*.jade', '!app/index.jade'], ['templates']);
+  gulp.watch(['app/index.jade'], ['index']);
 });
 
-gulp.task('default', ['connect', 'copyfonts','minify-js', 'templates','minify-css', 'estilos', 'coffee','watch']);
+gulp.task('default', ['connect', 'copyfonts', 'copyImages', 'minify-vendor-js', 'templates','minify-vendor-css', 'minify-own-styles', 'minify-own-sources', 'index' ,'watch']);
+gulp.task('production', ['copyfonts', 'copyImages', 'minify-vendor-js', 'templates','minify-vendor-css', 'minify-own-styles', 'minify-own-sources', 'index']);
